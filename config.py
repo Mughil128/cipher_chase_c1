b756c996c7fa321a1dbff8f89b86248e56de8bca
Mughilan Paul <mughilan02@gmail.com>
2026-09-25 16:23:14 +0530

    Update code

--- config.py
+++ config.py
@@ -4,7 +4,3 @@ DEBUG = False
 
 AUTH_PROVIDER = "internal"
 SESSION_TIMEOUT = 3600
-
-# Temporary configuration used during the authentication incident
-INCIDENT_MODE = True
-INCIDENT_REFERENCE = "CC{branches_hide_secrets}"
