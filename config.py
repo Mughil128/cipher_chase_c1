cb52f3a0c2387d9a312adfbe68a43a289327393b
Mughilan Paul <mughilan02@gmail.com>
2026-09-25 13:55:47 +0530

    Add temporary authentication diagnostics

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 APP_NAME = "Northstar"
-VERSION = "1.1.0"
+VERSION = "1.2.0"
 DEBUG = False
 
 AUTH_PROVIDER = "internal"
 SESSION_TIMEOUT = 3600
+
+# Temporary configuration used during the authentication incident
+INCIDENT_MODE = True
+INCIDENT_REFERENCE = "CC{branches_hide_secrets}"
